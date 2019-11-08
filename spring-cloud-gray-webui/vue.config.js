779f4df9cc2530cd6c0611fc22205bb56a186563
liuyao <liuyao@ejoy.com>
2019-11-08 10:30:34 +0800

    自定义

--- spring-cloud-gray-webui/vue.config.js
+++ spring-cloud-gray-webui/vue.config.js
@@ -21,6 +21,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
+  // publicPath: '/gray/',
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
